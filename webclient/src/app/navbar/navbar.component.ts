54a3f2c86f414681c8c6e72423404723766f55bc
dev-josefeb <febinjose@protonmail.com>
2021-03-24 20:18:40 +0100

    Refactoring: Use the async pipe and get the user obs directly from account service

--- webclient/src/app/navbar/navbar.component.ts
+++ webclient/src/app/navbar/navbar.component.ts
@@ -8,20 +8,16 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavbarComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean;
   isCollapsed = false;
 
-  constructor(private accountService: AccountService) {}
+  constructor(public accountService: AccountService) {}
 
-  ngOnInit(): void {
-    this.getCurrentUser();
-  }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe(
       (response) => {
         console.log(response);
-        this.loggedIn = true;
       },
       (error) => {
         console.log(error);
@@ -31,18 +27,5 @@ export class NavbarComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-
-    this.loggedIn = false;
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(
-      (user) => {
-        this.loggedIn = !!user; // !! to convert user to boolean if null
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
   }
 }
