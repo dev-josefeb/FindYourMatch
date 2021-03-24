6f5c2942834c67c2fa3ec2e9b4070c678ce47328
dev-josefeb <febinjose@protonmail.com>
2021-03-24 08:30:02 +0100

    Add angular forms and bindings for login

--- webclient/src/app/navbar/navbar.component.ts
+++ webclient/src/app/navbar/navbar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    console.log(this.model);
   }
-
 }
