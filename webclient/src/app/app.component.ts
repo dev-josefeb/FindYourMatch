03eb6cd215c0eb618fda22feb20042dbaf4de830
dev-josefeb <febinjose@protonmail.com>
2021-03-24 20:06:40 +0100

    Add persistence to the user login

--- webclient/src/app/app.component.ts
+++ webclient/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,16 @@ export class AppComponent implements OnInit {
   title = 'FindYourMatch';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService, private http: HttpClient) {}
 
   ngOnInit() {
     this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 
   getUsers() {
