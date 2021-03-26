2657fab3812651f61bfb95c5237b50bf015504d3
dev-josefeb <febinjose@protonmail.com>
2021-03-26 18:50:37 +0100

    Add Member and Photo Dtos

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class PhotoDto
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsMain { get; set; }
+    }
+}
