61702cab870c48177d9020f680f3a9237eb250e1
dev-josefeb <febinjose@protonmail.com>
2021-03-26 09:47:06 +0100

    Extend the user entity  and add the photo entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,10 +1,39 @@
+using System;
+using System.Collections.Generic;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
+
         public string UserName { get; set; }
+
         public byte[] PasswordHash { get; set; }
+
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string KnownAs { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
     }
 }
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsMain { get; set; }
+
+        public string PublicId { get; set; }
+    }
+}
