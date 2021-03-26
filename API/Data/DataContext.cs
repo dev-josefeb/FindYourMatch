365d53b150afb07d75f1c7f282ca92591f751918
dev-josefeb <febinjose@protonmail.com>
2021-03-26 10:15:46 +0100

    Create a relation between the AppUser and Photo and update the migration in db

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -5,9 +5,10 @@ namespace API.Data
 {
     public class DataContext : DbContext
     {
-        public DbSet<AppUser> Users { get; set; }
         public DataContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<AppUser> Users { get; set; }
     }
 }
--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,5 +1,8 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
@@ -9,5 +12,9 @@
         public bool IsMain { get; set; }
 
         public string PublicId { get; set; }
+
+        public AppUser AppUser { get; set; }
+
+        public int AppUserId { get; set; }
     }
 }
