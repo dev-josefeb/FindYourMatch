a257fc202492dbe90dc75ccd1934cabe24869cce
dev-josefeb <febinjose@protonmail.com>
2021-03-26 16:46:59 +0100

    Create and implement the UserRepository

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+
+        Task<bool> SaveAllAsync();
+
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+
+        Task<AppUser> GetUserByIdAsync(int id);
+
+        Task<AppUser> GetUserByUsernameAsync(string username);
+
+    }
+}
