6ba2268f59bd069c5945c0e27768fa04b333aa5c
dev-josefeb <febinjose@protonmail.com>
2021-03-27 11:24:11 +0100

    Add routing to the member-detail page when clicking on a member

--- webclient/src/app/members/member-detail/member-detail.component.ts
+++ webclient/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe((member) => (this.member = member));
+  }
 }
