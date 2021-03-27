b215bad5c4648f403d8eab83e2ba1f8f69570494
dev-josefeb <febinjose@protonmail.com>
2021-03-27 08:32:15 +0100

    Add a member service and set baseUrl in env variables

---
+++ webclient/src/app/_services/members.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'bearer' + JSON.parse(localStorage.getItem('user')).token,
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getMembers() {
+    this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users' + username, httpOptions);
+  }
+}
