9b528d63fd4ed60095268508b466b5450e27445b
dev-josefeb <febinjose@protonmail.com>
2021-03-27 16:55:32 +0100

    Add component for members to edit their profile

---
+++ webclient/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css'],
+})
+export class MemberEditComponent implements OnInit {
+  member: Member;
+  user: User;
+
+  constructor(private accountService: AccountService, private membersService: MembersService) {
+    this, accountService.currentUser$.pipe(take(1)).subscribe((user) => (this.user = user));
+  }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.membersService.getMember(this.user.username).subscribe((member) => (this.member = member));
+  }
+}
