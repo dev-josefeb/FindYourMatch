f1795c4144532c97cd49ad9c3ce6bb4d55c9a56b
dev-josefeb <febinjose@protonmail.com>
2021-03-28 17:52:44 +0200

    Modify the UsersController to return a 201 request on photo upload with the correct location header

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -33,7 +33,7 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -73,7 +73,10 @@ namespace API.Controllers
             user.Photos.Add(photo);
 
             if (await _userRepository.SaveAllAsync())
-                return _mapper.Map<PhotoDto>(photo);
+            {
+                return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+
+            }
 
             return BadRequest("Problem adding photo");
         }
