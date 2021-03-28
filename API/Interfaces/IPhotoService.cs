9c3150799e9ad8b669940fcf7538dc0ca49184d4
dev-josefeb <febinjose@protonmail.com>
2021-03-28 12:00:16 +0200

    Add photo service in the api for cloudinary

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,13 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
