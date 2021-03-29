fd29ebb4b0520a32dcaf153c8cde9cbdab2f69c0
dev-josefeb <febinjose@protonmail.com>
2021-03-29 20:52:03 +0200

    Add all helper classes required for pagination ( Default value of pages and to Add custom header to response)

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System.Text.Json;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static StringValues JSON { get; private set; }
+
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
