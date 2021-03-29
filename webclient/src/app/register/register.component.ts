d1bb95f8925b4bebaa0f68c89b3b47d289184e07
dev-josefeb <febinjose@protonmail.com>
2021-03-29 10:18:35 +0200

    Add custom validator for confirming input password

--- webclient/src/app/register/register.component.ts
+++ webclient/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -24,8 +24,10 @@ export class RegisterComponent implements OnInit {
     this.registerForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
     });
+
+    this.registerForm.controls.password.valueChanges?.subscribe(() => this.registerForm.controls.confirmPassword.updateValueAndValidity());
   }
 
   register() {
@@ -42,5 +44,11 @@ export class RegisterComponent implements OnInit {
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value ? null : { isMatching: true };
+    };
   }
 }
