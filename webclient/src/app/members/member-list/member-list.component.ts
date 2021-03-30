bca46494cc07941ba386fbcad35497cebca6cb3a
dev-josefeb <febinjose@protonmail.com>
2021-03-30 13:57:56 +0200

    Add pagination filter form to the client page

--- webclient/src/app/members/member-list/member-list.component.ts
+++ webclient/src/app/members/member-list/member-list.component.ts
@@ -17,6 +17,10 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' },
+  ];
 
   constructor(private memberService: MembersService, private accountService: AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
@@ -38,6 +42,11 @@ export class MemberListComponent implements OnInit {
 
   pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
+    this.loadMembers();
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.user);
     this.loadMembers();
   }
 }
