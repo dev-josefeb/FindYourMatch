0353e6ccb35afffd90e8224447b6ca175e7f8f4d
dev-josefeb <febinjose@protonmail.com>
2021-03-30 20:18:41 +0200

    Refactoring: Changing the userbyusername to userbyid in the loguseractivity to optimise db return(Not including photos)

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -3,8 +3,6 @@ using API.Interfaces;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Helpers
@@ -16,9 +14,9 @@ namespace API.Helpers
             var resultContext = await next();
             if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-            var username = resultContext.HttpContext.User.GetUsername();
+            var userId = resultContext.HttpContext.User.GetUserId();
             var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
-            var user = await repo.GetUserByUsernameAsync(username);
+            var user = await repo.GetUserByIdAsync(userId);
             user.LastActive = DateTime.Now;
             await repo.SaveAllAsync();
         }
