7ca3195004f044fb79b99f1903864bcf488c9e40
dev-josefeb <febinjose@protonmail.com>
2021-03-30 16:23:31 +0200

    Styling: Improve card ux and general styling fixes

--- webclient/src/app/members/member-card/member-card.component.ts
+++ webclient/src/app/members/member-card/member-card.component.ts
@@ -8,8 +8,13 @@ import { Member } from 'src/app/_models/member';
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: Member;
+  imageLoading: boolean = true;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onLoad() {
+    this.imageLoading = false;
+  }
 }
