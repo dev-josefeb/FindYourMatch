0c9f38197ff2b05f632438a533e452204e256ab1
dev-josefeb <febinjose@protonmail.com>
2021-03-30 12:45:57 +0200

    Add Gender to the UserDto to avoid a second API call from the client

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,7 +46,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -71,7 +72,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
 
         }
