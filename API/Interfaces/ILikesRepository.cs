053f11212a14b45d96202ce76041d1a7316f4d5b
dev-josefeb <febinjose@protonmail.com>
2021-03-31 15:40:23 +0200

    Add the likes repository interface and like dto

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using API.DTOs;
+using API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+
+        Task<AppUser> GetUserWithLikes(int userId);
+
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+
+    }
+}
