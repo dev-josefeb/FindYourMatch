8c8f18e82055d1c10a82900a72796260c56e02b6
dev-josefeb <febinjose@protonmail.com>
2021-04-01 19:19:43 +0200

    Set up the Message Dto and Message Repository

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,23 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+
+        void DeleteMessage(Message message);
+
+        Task<Message> GetMessage(int id);
+
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentId, int recipientId);
+
+        Task<bool> SaveAllAsync();
+    }
+}
