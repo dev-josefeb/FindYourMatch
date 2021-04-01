38cb3e0d9da2f9f5d5e86ce097d63b54ec04a697
dev-josefeb <febinjose@protonmail.com>
2021-04-01 19:56:39 +0200

    Add migration to correct SenderUsername datatype from int to string

---
+++ API/Data/Migrations/20210401175427_MessageEntityCorrectSenderUsername.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class MessageEntityCorrectSenderUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SenderUsername",
+                table: "Messages",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SenderUsername",
+                table: "Messages",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
