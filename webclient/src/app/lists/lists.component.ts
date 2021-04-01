515ede6c053d5d40347444612545a35c0265c6fa
dev-josefeb <febinjose@protonmail.com>
2021-04-01 09:58:33 +0200

    Implement pagination on the likes page in the client

--- webclient/src/app/lists/lists.component.ts
+++ webclient/src/app/lists/lists.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../_models/member';
+import { Pagination } from '../_models/pagination';
 import { MembersService } from '../_services/members.service';
 
 @Component({
@@ -9,6 +10,9 @@ import { MembersService } from '../_services/members.service';
 })
 export class ListsComponent implements OnInit {
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 12;
+  pagination: Pagination;
 
   //Partial so that each property in Member is optional
   members: Partial<Member[]>;
@@ -20,6 +24,14 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes() {
-    this.memberService.getLikes(this.predicate).subscribe((response) => (this.members = response));
+    this.memberService.getLikes(this.predicate, this.pageNumber, this.pageSize).subscribe((response) => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    });
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadLikes();
   }
 }
