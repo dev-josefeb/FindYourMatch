2cffc1c4c37931ab072abc0b32a583687e1911bf
dev-josefeb <febinjose@protonmail.com>
2021-04-01 08:54:16 +0200

    Implement pagination for the likes in the API

---
+++ API/Helpers/LikesParams.cs
@@ -0,0 +1,10 @@
+﻿
+namespace API.Helpers
+{
+    public class LikesParams : PaginationParams
+    {
+        public int UserId { get; set; }
+
+        public string Predicate { get; set; }
+    }
+}
