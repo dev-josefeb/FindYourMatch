ca9a3fd28aba183a940d5b31918bf1a8dbf76415
dev-josefeb <febinjose@protonmail.com>
2021-04-01 19:19:43 +0200

    Set up the Message Dto and Message Repository

---
+++ API/Data/MessageRepository.cs
@@ -0,0 +1,51 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly DataContext _context;
+
+        public MessageRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void AddMessage(Message message)
+        {
+            _context.Messages.Add(message);
+        }
+
+        public void DeleteMessage(Message message)
+        {
+            _context.Messages.Remove(message);
+        }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FindAsync(id);
+        }
+
+        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MessageDto>> GetMessageThread(int currentId, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
