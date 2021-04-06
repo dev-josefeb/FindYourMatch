c7c2c524a845ba84df62907c18ce169bfebcb9b5
dev-josefeb <febinjose@protonmail.com>
2021-04-06 07:57:32 +0200

    Display a basic message thread in the client

--- webclient/src/app/_services/message.service.ts
+++ webclient/src/app/_services/message.service.ts
@@ -18,4 +18,8 @@ export class MessageService {
 
     return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
   }
+
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+  }
 }
