c5c2896571058be0550751857c91784099e9f998
dev-josefeb <febinjose@protonmail.com>
2021-04-07 12:57:46 +0200

    Activate/load messages from the Api endpoint only on opening the messages tab

--- webclient/src/app/members/member-detail/member-detail.component.ts
+++ webclient/src/app/members/member-detail/member-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -10,11 +13,15 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs') memberTabs: TabsetComponent;
+
   member: Member;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
+  activeTab: TabDirective;
+  messages: Message[] = [];
 
-  constructor(private memberService: MembersService, private route: ActivatedRoute) {}
+  constructor(private memberService: MembersService, private route: ActivatedRoute, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.loadMember();
@@ -41,5 +48,14 @@ export class MemberDetailComponent implements OnInit {
       this.member = member;
       this.galleryImages = this.getImages();
     });
+  }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (this.activeTab.heading === 'Messages' && this.messages.length === 0) this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessageThread(this.member.username).subscribe((messages) => (this.messages = messages));
   }
 }
--- webclient/src/app/members/member-messages/member-messages.component.ts
+++ webclient/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Message } from 'src/app/_models/message';
-import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-messages',
@@ -8,17 +7,9 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css'],
 })
 export class MemberMessagesComponent implements OnInit {
-  @Input() username: string;
+  @Input() messages: Message[];
 
-  messages: Message[];
+  constructor() {}
 
-  constructor(private messageService: MessageService) {}
-
-  ngOnInit(): void {
-    this.loadMessages();
-  }
-
-  loadMessages() {
-    this.messageService.getMessageThread(this.username).subscribe((messages) => (this.messages = messages));
-  }
+  ngOnInit(): void {}
 }
