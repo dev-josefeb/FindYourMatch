74940cfa61bf61af4cb1b93fd734ce2bf0d2e3b7
dev-josefeb <febinjose@protonmail.com>
2021-04-08 12:59:22 +0200

    Add feature to delete user messages

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -46,9 +46,9 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(user => user.Recipient.UserName == messageParams.Username),
-                "Outbox" => query.Where(user => user.Sender.UserName == messageParams.Username),
-                _ => query.Where(user => user.Sender.UserName == messageParams.Username && user.DateRead == null)
+                "Inbox" => query.Where(user => user.Recipient.UserName == messageParams.Username && user.RecipientDeleted == false),
+                "Outbox" => query.Where(user => user.Sender.UserName == messageParams.Username && user.SenderDeleted == false),
+                _ => query.Where(user => user.Sender.UserName == messageParams.Username && user.RecipientDeleted == false && user.DateRead == null)
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -61,10 +61,10 @@ namespace API.Data
             var messages = await _context.Messages
                 .Include(user => user.Sender).ThenInclude(photo => photo.Photos)
                 .Include(user => user.Recipient).ThenInclude(photo => photo.Photos)
-                .Where(message => message.Recipient.UserName == currentUsername
+                .Where(message => message.Recipient.UserName == currentUsername && message.RecipientDeleted == false
                                && message.Sender.UserName == recipientUsername
                                || message.Recipient.UserName == recipientUsername
-                               && message.Sender.UserName == currentUsername)
+                               && message.Sender.UserName == currentUsername && message.SenderDeleted == false)
                 .OrderBy(message => message.MessageSent)
                 .ToListAsync();
 
