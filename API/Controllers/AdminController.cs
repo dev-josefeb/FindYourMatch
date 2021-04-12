3b7aaedc83dc16cb46d39ec8cdca2b6132a275cc
dev-josefeb <febinjose@protonmail.com>
2021-04-12 14:53:27 +0200

    Add policy based authorization

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("users-with-roles")]
+        public ActionResult GetUsersWithRoles()
+        {
+            return Ok("Only admins can see this");
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photos-to-moderate")]
+        public ActionResult GetPhotosForModeration()
+        {
+            return Ok("Only admins or moderators can see this");
+        }
+    }
+}
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -36,6 +36,12 @@ namespace API.Extensions
                   };
               });
 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
             return services;
         }
     }
