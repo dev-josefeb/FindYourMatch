03590ec3d697a26790cabe1689a95d8afc1215f4
dev-josefeb <febinjose@protonmail.com>
2021-04-12 20:37:47 +0200

    Create custom directive to hide or show admin navlink to only admin or moderator users

--- webclient/src/app/app.module.ts
+++ webclient/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DateInputComponent } from './_forms/date-input/date-input.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
 
 @NgModule({
   declarations: [
@@ -49,6 +50,7 @@ import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
     DateInputComponent,
     MemberMessagesComponent,
     AdminPanelComponent,
+    HasRoleDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, SharedModule],
   providers: [
