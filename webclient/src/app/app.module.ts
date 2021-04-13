791da10d04cbba57d00c15eab584f9b7c60ad2d1
dev-josefeb <febinjose@protonmail.com>
2021-04-13 20:09:26 +0200

    Add a basic modal component that is displayed for editing the user roles

--- webclient/src/app/app.module.ts
+++ webclient/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
 import { UserManagementComponent } from './admin/user-management/user-management.component';
 import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 @NgModule({
   declarations: [
@@ -55,6 +56,7 @@ import { PhotoManagementComponent } from './admin/photo-management/photo-managem
     HasRoleDirective,
     UserManagementComponent,
     PhotoManagementComponent,
+    RolesModalComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, SharedModule],
   providers: [
