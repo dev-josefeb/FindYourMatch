dc2d011877e74e3c0a7e68445f1c08617332db9a
dev-josefeb <febinjose@protonmail.com>
2021-04-15 09:47:42 +0200

    Add authenticating to SignalR

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,7 +35,7 @@ namespace API
 
             app.UseHttpsRedirection();
             app.UseRouting();
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:4200"));
             app.UseAuthentication();
             app.UseAuthorization();
 
