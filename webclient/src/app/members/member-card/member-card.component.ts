a30a2831710c67fbde2f6a76feb8bedd02a7c810
dev-josefeb <febinjose@protonmail.com>
2021-04-15 20:29:16 +0200

    Add indicator to display the users online presence

--- webclient/src/app/members/member-card/member-card.component.ts
+++ webclient/src/app/members/member-card/member-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
+import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
   selector: 'app-member-card',
@@ -12,7 +13,7 @@ export class MemberCardComponent implements OnInit {
   @Input() member: Member;
   imageLoading: boolean = true;
 
-  constructor(private memberService: MembersService, private toastr: ToastrService) {}
+  constructor(private memberService: MembersService, private toastr: ToastrService, public presence: PresenceService) {}
 
   ngOnInit(): void {}
 
