07283a868bea542c4a8229be3ee68894d2ae96df
dev-josefeb <febinjose@protonmail.com>
2021-04-16 20:45:39 +0200

    Add functionality to track message groups by using the DB

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -15,6 +15,10 @@ namespace API.Data
 
         public DbSet<Message> Messages { get; set; }
 
+        public DbSet<Group> Groups { get; set; }
+
+        public DbSet<Connection> Connections { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
