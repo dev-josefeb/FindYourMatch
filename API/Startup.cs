05d7630d3bc898ebf258d088b64d3acd978ddb99
dev-josefeb <febinjose@protonmail.com>
2021-04-16 17:30:26 +0200

    Created a message hub on the api

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,6 +43,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
