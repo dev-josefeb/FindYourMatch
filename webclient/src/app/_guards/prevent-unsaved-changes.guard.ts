aec3f86f30e9adf2c4c22acdb2190cdead3bd642
dev-josefeb <febinjose@protonmail.com>
2021-04-17 21:26:53 +0200

    Replace default js confirm dialog with a custom service based dialog

--- webclient/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ webclient/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(component: MemberEditComponent): boolean {
+  constructor(private confirmService: ConfirmService) {}
+
+  canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
     if (component.editForm?.dirty) {
-      return confirm('Are you sure you want to continue? Any unsaved unchanges will be lost');
+      return this.confirmService.confirm();
     }
     return true;
   }
--- webclient/src/app/messages/messages.component.ts
+++ webclient/src/app/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Message } from '../_models/message';
 import { Pagination } from '../_models/pagination';
+import { ConfirmService } from '../_services/confirm.service';
 import { MessageService } from '../_services/message.service';
 
 @Component({
@@ -15,7 +16,7 @@ export class MessagesComponent implements OnInit {
   pageNumber = 1;
   pageSize = 10;
 
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService, private confirmService: ConfirmService) {}
 
   ngOnInit(): void {
     this.loadMessages();
@@ -30,12 +31,16 @@ export class MessagesComponent implements OnInit {
   }
 
   deleteMessage(id: number) {
-    this.messageService.deleteMessage(id).subscribe(() =>
-      this.messages.splice(
-        this.messages.findIndex((m) => m.id === id),
-        1
-      )
-    );
+    this.confirmService.confirm('Confirm Delete Message', 'This cannot be undone').subscribe((result) => {
+      if (result) {
+        this.messageService.deleteMessage(id).subscribe(() =>
+          this.messages.splice(
+            this.messages.findIndex((m) => m.id === id),
+            1
+          )
+        );
+      }
+    });
   }
 
   pageChanged(event: any) {
