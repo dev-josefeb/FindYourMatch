685251a021b0caef92cb3f9d88e825972e4f7daf
dev-josefeb <febinjose@protonmail.com>
2021-04-17 19:34:27 +0200

    Show unread message count on the navbar

--- webclient/src/app/navbar/navbar.component.ts
+++ webclient/src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
+import { MessageService } from '../_services/message.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,10 +12,15 @@ import { AccountService } from '../_services/account.service';
 export class NavbarComponent implements OnInit {
   model: any = {};
   isCollapsed = true;
+  unreadMessages = 0;
 
-  constructor(public accountService: AccountService, private toastr: ToastrService, private router: Router) {}
+  constructor(public accountService: AccountService, private toastr: ToastrService, private router: Router, private messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.messageService.getMessages(1, 10, 'Unread').subscribe((response) => {
+      this.unreadMessages = response.result.length;
+    });
+  }
 
   login() {
     this.accountService.login(this.model).subscribe(() => this.router.navigateByUrl('/members'));
