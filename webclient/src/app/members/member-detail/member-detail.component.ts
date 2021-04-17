6689e69e1895aa0122c738aee96f61e484b833c5
dev-josefeb <febinjose@protonmail.com>
2021-04-17 21:41:05 +0200

    Finish wiring the like user button in the user profile page

--- webclient/src/app/members/member-detail/member-detail.component.ts
+++ webclient/src/app/members/member-detail/member-detail.component.ts
@@ -2,11 +2,13 @@ import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
 import { Message } from 'src/app/_models/message';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
 import { PresenceService } from 'src/app/_services/presence.service';
 
@@ -25,7 +27,7 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
   messages: Message[] = [];
   user: User;
 
-  constructor(public presence: PresenceService, private route: ActivatedRoute, private messageService: MessageService, private accountService: AccountService, private router: Router) {
+  constructor(public presence: PresenceService, private route: ActivatedRoute, private messageService: MessageService, private accountService: AccountService, private router: Router, private memberService: MembersService, private toastr: ToastrService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe((user) => (this.user = user));
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
@@ -63,6 +65,10 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
 
   loadMessages() {
     this.messageService.getMessageThread(this.member.username).subscribe((messages) => (this.messages = messages));
+  }
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.username).subscribe(() => this.toastr.success('You have liked ' + member.knownAs));
   }
 
   ngOnDestroy(): void {
