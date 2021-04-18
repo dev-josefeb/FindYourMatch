b6d2254590c55b8cf2cf267d42c9307b9a7ba882
dev-josefeb <febinjose@protonmail.com>
2021-04-18 11:35:34 +0200

    Publishing: Remove delay introduced for testing

--- webclient/src/app/_interceptors/loading.interceptor.ts
+++ webclient/src/app/_interceptors/loading.interceptor.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BusyService } from '../_services/busy.service';
-import { delay, finalize } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
@@ -10,9 +10,6 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
-    return next.handle(request).pipe(
-      delay(1000),
-      finalize(() => this.busyService.idle())
-    );
+    return next.handle(request).pipe(finalize(() => this.busyService.idle()));
   }
 }
