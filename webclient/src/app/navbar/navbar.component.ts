c7b43670eee5e33c24efecdd28bfe5c019ae012f
dev-josefeb <febinjose@protonmail.com>
2021-04-20 18:27:34 +0200

    BugFix: Prevent unauthorized toast message on refreshing the home page when not logged in

--- webclient/src/app/navbar/navbar.component.ts
+++ webclient/src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 import { MessageService } from '../_services/message.service';
 
@@ -17,9 +19,14 @@ export class NavbarComponent implements OnInit {
   constructor(public accountService: AccountService, private toastr: ToastrService, private router: Router, private messageService: MessageService) {}
 
   ngOnInit(): void {
-    this.messageService.getMessages(1, 10, 'Unread').subscribe((response) => {
-      this.unreadMessages = response.result.length;
-    });
+    let currentUser: User;
+
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user) => (currentUser = user));
+    if (currentUser) {
+      this.messageService.getMessages(1, 10, 'Unread').subscribe((response) => {
+        this.unreadMessages = response.result.length;
+      });
+    }
   }
 
   login() {
